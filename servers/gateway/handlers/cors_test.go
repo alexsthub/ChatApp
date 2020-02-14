2998b255d34f816a76838723b20ab17e9536ce34
Alex Tan <alextan785@gmail.com>
2020-02-13 16:11:55 -0800

    cors tests

--- servers/gateway/handlers/cors_test.go
+++ servers/gateway/handlers/cors_test.go
@@ -1,50 +1,31 @@
 package handlers
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-var expectedHeaders = []string{
-	"Access-Control-Allow-Origin",
-	"Access-Control-Allow-Headers",
-	"Access-Control-Expose-Headers",
-	"Access-Control-Max-Age",
+var expectedHeaders = map[string]string{
+	"Access-Control-Allow-Origin":   "*",
+	"Access-Control-Allow-Methods":  "GET, PUT, POST, PATCH, DELETE",
+	"Access-Control-Allow-Headers":  "Content-Type, Authorization",
+	"Access-Control-Expose-Headers": "Authorization",
+	"Access-Control-Max-Age":        "600",
 }
 
-func TestGET(t *testing.T) {
-	// signingKey := os.Getenv("SESSIONKEY")
-	// redisAddr := os.Getenv("REDISADDR")
-	// dsn := os.Getenv("DSN")
+func TestCors(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	cors := CorsMW{h}
 
-	// sessionStore := sessions.NewRedisStore(redis.NewClient(&redis.Options{Addr: redisAddr}), time.Hour)
-	// if sessionStore == nil {
-	// 	log.Fatal("Cannot connect to session store")
-	// }
-	// userStore, err := users.NewSQLStore(dsn)
-	// if err != nil {
-	// 	log.Fatal("Cannot connect to users database, reason: ", err)
-	// }
-	// ctx := &ContextHandler{
-	// 	SigningKey:   signingKey,
-	// 	SessionStore: sessionStore,
-	// 	UserStore:    userStore,
-	// }
-	// mux := http.NewServeMux()
-	// wrappedMuxed := &CorsMW{Handler: mux}
-
-	// req := httptest.NewRequest("GET", "http://example.com/foo", nil)
-	// w := httptest.NewRecorder()
-
-}
-
-func TestPOST(t *testing.T) {
-
-}
-
-func TestPATCH(t *testing.T) {
-
-}
-
-func TestDELETE(t *testing.T) {
-
+	req := httptest.NewRequest("GET", "https://api.alexst.me/v1/sessions", nil)
+	w := httptest.NewRecorder()
+	cors.ServeHTTP(w, req)
+	resp := w.Result()
+	for key, value := range expectedHeaders {
+		seenHeader := resp.Header.Get(key)
+		if seenHeader != value {
+			t.Errorf("Header: %s value is %s but should be %s", key, seenHeader, value)
+		}
+	}
 }
