1de6a4d8bd80bab3c9834e67d133210ef2d9b274
Alex Tan <alextan785@gmail.com>
2020-02-14 14:53:23 -0800

    implement trie without testing

--- servers/gateway/indexes/trie.go
+++ servers/gateway/indexes/trie.go
@@ -1,4 +1,162 @@
 package indexes
 
-//TODO: implement a trie data structure that stores
-//keys of type string and values of type int64
+import (
+	"sort"
+	"sync"
+)
+
+//TODO: implement a trie data structure that stores keys of type string and values of type int64
+
+//PRO TIP: if you are having troubles and want to see
+//what your trie structure looks like at various points,
+//either use the debugger, or try this package:
+//https://github.com/davecgh/go-spew
+
+//Trie implements a trie data structure mapping strings to int64s
+//that is safe for concurrent use.
+type Trie struct {
+	root *Node
+	size int
+	mu   sync.Mutex
+}
+
+// Node is a node in the Trie
+type Node struct {
+	key      rune
+	value    []int64
+	children map[rune]*Node
+	parent   *Node
+}
+
+//NewTrie constructs a new Trie.
+func NewTrie() *Trie {
+	return &Trie{root: &Node{}}
+}
+
+//Len returns the number of entries in the trie.
+func (t *Trie) Len() int {
+	return t.size
+}
+
+//Add adds a key and value to the trie.
+func (t *Trie) Add(key string, value int64) {
+	t.mu.Lock()
+	currentNode := t.root
+	runes := []rune(key)
+	for i := range runes {
+		char := runes[i]
+		if _, exists := currentNode.children[char]; !exists {
+			newNode := &Node{
+				parent: currentNode,
+				key:    char,
+			}
+			currentNode.children[char] = newNode
+		}
+		currentNode = currentNode.children[char]
+	}
+	// If the value does not exist in the set
+	if !Contains(currentNode.value, value) {
+		currentNode.value = append(currentNode.value, value)
+		t.size++
+	}
+	t.mu.Unlock()
+}
+
+// RuneSlice is a helper to sort slice of runes
+type RuneSlice []rune
+
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// Contains takes a slice and looks for an element in it. If found it will true
+func Contains(slice []int64, val int64) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+//Find finds `max` values matching `prefix`. If the trie
+//is entirely empty, or the prefix is empty, or max == 0,
+//or the prefix is not found, this returns a nil slice.
+func (t *Trie) Find(prefix string, max int) []int64 {
+	t.mu.Lock()
+	var results []int64
+	currentNode := t.root
+	if t.Len() == 0 || prefix == "" || max == 0 {
+		return results
+	}
+	runes := []rune(prefix)
+	for i := range runes {
+		char := runes[i]
+		if nextNode, exists := currentNode.children[char]; exists {
+			currentNode = nextNode
+		} else {
+			return results
+		}
+	}
+	// Made it to the end of the prefix. DFS to get values up to `max`
+	queue := []*Node{currentNode}
+	for len(queue) > 0 {
+		currentNode = queue[0]
+		queue = queue[1:]
+		// If value exists add to results
+		for v := range currentNode.value {
+			results = append(results, int64(v))
+			if len(results) >= max {
+				return results
+			}
+		}
+		// Sort keys alphabetically and BFS
+		keys := make([]rune, 0)
+		for k := range currentNode.children {
+			keys = append(keys, k)
+		}
+		sort.Sort(RuneSlice(keys))
+		for _, k := range keys {
+			nextNode, _ := currentNode.children[k]
+			queue = append(queue, nextNode)
+		}
+	}
+	t.mu.Unlock()
+	return results
+}
+
+//Remove removes a key/value pair from the trie
+//and trims branches with no values.
+func (t *Trie) Remove(key string, value int64) {
+	t.mu.Lock()
+	currentNode := t.root
+	runes := []rune(key)
+	for i := range runes {
+		char := runes[i]
+		if nextNode, exists := currentNode.children[char]; exists {
+			currentNode = nextNode
+		}
+	}
+	// Current node is the node to remove value from
+	var index int
+	valueList := currentNode.value
+	for i, v := range valueList {
+		if v == value {
+			index = int(i)
+		}
+	}
+	valueList = append(valueList[0:index], valueList[index+1:]...)
+	currentNode.value = valueList
+	t.size--
+	// If node is leafNode && valueList is empty, trim up until there is a value
+	for {
+		if len(currentNode.children) == 0 && len(currentNode.value) == 0 {
+			delNode := currentNode
+			currentNode = currentNode.parent
+			delete(currentNode.children, delNode.key)
+		} else {
+			break
+		}
+	}
+	t.mu.Unlock()
+}
